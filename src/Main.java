7e18770f83a3f008b23add886d172f04a85f1012
Sajeev <codersaj09@gmail.com>
2021-11-05 20:59:48 -0400

    added basic operation and input :)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner epic = new Scanner(System.in);
+        Operation hello = new Operation();
 
+        double a = epic.nextDouble();
+        double b = epic.nextDouble();
+      //  int c = epic.nextInt();
+        System.out.println(hello.add(a,b));
     }
 }
---
+++ src/Operation.java
@@ -0,0 +1,21 @@
+public class Operation {
+    public Operation() {
+
+    }
+    public double add(double a, double b){
+        return a+b;
+    }
+    public double sub(double a, double b){
+        return a-b;
+    }
+    public double divide(double a, double b){
+        return a/b;
+    }
+    public double multi( double a, double b){
+        return a*b;
+    }
+    public double modulus( double a, double b){
+        return a%b;
+    }
+
+}
